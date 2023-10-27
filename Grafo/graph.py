540129fb9c09ff26e339d1237a57c0fc54bc87fe
bruno dias <diasbruno00@gmail.com>
2023-10-27 17:17:07 -0300

    add comentarios na classe grafo

--- Grafo/graph.py
+++ Grafo/graph.py
@@ -8,24 +8,24 @@ class Graph:
 
 
     def add_no(self,node):
-        if node not in self.adj:
+        if node not in self.adj: # caso não tenha o no ele irar criar
             self.adj[node] = {}
             self.num_nodes += 1
 
     def add_edges(self, u, v, weight):
-        if u not in self.adj:
+        if u not in self.adj: # caso não tenha o no U ele irar criar
             self.add_no(u)
 
-        if v not in self.adj:
+        if v not in self.adj: # caso não tenha o no V ele irar criar
             self.add_no(v)
 
-        self.adj[u][v] = weight
+        self.adj[u][v] = weight # se ele não caiu nos if e pq existe um no U e no V entao ele irar relacionar o no U com o no V
         self.num_edges += 1  
     
 
-    def add_twe_way_edge(self, u , v, weigtht):
+    def add_twe_way_edge(self, u , v, weigtht): # aqui irar fazer a mesma verificação do add_egnes so que ele vai relacionar U com V e V com U
 
-        self.add_edges(u,v,weigtht)
+        self.add_edges(u,v,weigtht)  
 
         self.add_edges(v,u,weigtht)
     
--- Grafo/main.py
+++ Grafo/main.py
@@ -2,10 +2,6 @@ from graph import Graph
 
 
 g = Graph()
-g.add_no("Ana")
-g.add_no("Bia")
-g.add_no("Caio")
-g.add_no("Duda")
 
 
 g.add_twe_way_edge("Bia", "Ana", 0)
